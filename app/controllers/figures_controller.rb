ddd67d114424430c667567932846339cb09ce337
JDumadag25 <justin.dumadag@gmail.com>
2018-04-05 14:52:59 -0400

    filled in figures controller

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,30 @@
+class FiguresController < ApplicationController
+
+get '/figures' do
+end
+
+get '/figures/new'
+end
+
+post '/figures' do
+redirect to '/figures/:id'
+end
+
+get '/figures/:id'
+end
+
+get '/figures/:id/edit'
+end
+
+patch '/figures/:id'
+redirect to '/figures/:id'
+end
+
+delete '/figures/:id/delete'
+redirect to '/figures'
+end
+
+
+
+
+end
