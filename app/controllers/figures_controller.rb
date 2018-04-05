161362115b1d8e1e87e69d54c9c12b169ea264f9
JDumadag25 <justin.dumadag@gmail.com>
2018-04-05 14:56:42 -0400

    my bad

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,23 +4,24 @@ get '/figures' do
 end
 
 get '/figures/new'
+erb :new
 end
 
 post '/figures' do
 redirect to '/figures/:id'
 end
 
-get '/figures/:id'
+get '/figures/:id' do
 end
 
-get '/figures/:id/edit'
+get '/figures/:id/edit' do
 end
 
-patch '/figures/:id'
+patch '/figures/:id' do
 redirect to '/figures/:id'
 end
 
-delete '/figures/:id/delete'
+delete '/figures/:id/delete' do
 redirect to '/figures'
 end
 
