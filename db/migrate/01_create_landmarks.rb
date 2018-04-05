63da624c0d29db12c10e80c6c456cc2451403e84
J.D. Bean <jonathandbean@gmail.com>
2018-04-05 15:51:17 -0400

    FIXED

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -2,7 +2,7 @@ class CreateLandmarks < ActiveRecord::Migration[5.1]
   def change
     create_table :landmarks do |t|
       t.string :name
-      t.references :figures
+      t.integer :figure_id
       t.integer :year_completed
     end
   end
